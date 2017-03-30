2b0168c8bd0e64b08d654c38aa544b90a75ca392
eborghi10 <emiliano_borghi@hotmail.com>
2017-03-30 15:27:53 -0300

    Edit arduino_actuators

--- arduino_actuators/src/main/main.ino
+++ arduino_actuators/src/main/main.ino
@@ -1,6 +1,18 @@
 /**
  * http://answers.ros.org/question/232045/rosserial-multiple-publisher-and-subscriber/
  *
+ * > Upload to Arduino and then:
+ *
+ * $ roscore
+ * $ rosrun rosserial_python serial_node.py /dev/ttyACM0
+ *
+ * ---------------------------------------------------------------
+ *
+ * Or BETTER:
+ *
+ * 1) Upload to Arduino
+ * 2) Compile project in the ROS workspace (catkin_make)
+ * 3) $ roslaunch arduino_actuators execute.launch
  */
 
 #include "include/DifferentialDriveRobot.h"
@@ -14,8 +26,8 @@ DifferentialDriveRobot *my_robot;
 void setup()
 {
   my_robot = new DifferentialDriveRobot(
-    new DCMotor(4,5, new MagneticEncoder(10, LEFT, ZERO_TO_2PI)),
-    new DCMotor(6,7, new MagneticEncoder(11, RIGHT, ZERO_TO_2PI))
+    new DCMotor(4,5, new MagneticEncoder(10, LEFT, ZERO_TO_2PI), LEFT),
+    new DCMotor(6,7, new MagneticEncoder(11, RIGHT, ZERO_TO_2PI), RIGHT)
   );
 
   my_robot->UpdatePhysicalParameters(0.2, 1.0);
@@ -25,32 +37,12 @@ void setup()
 
 void loop() 
 {
-  (my_robot->nh).spinOnce();
-  
-  // TODO: PUT THE FOLLOWING INTO A FUNCTION
+	(my_robot->nh).loginfo("Initializing program...");
+	(my_robot->nh).spinOnce();
 
-  msg_angle.header.stamp  = my_robot->nh.now();
+  	my_robot->SendAngles();
 
-  dT = msg_angle.header.stamp.toSec() - prevTime.toSec();
-
-  msg_angle.name[LEFT]      = my_robot->GetEncoderTopicName(LEFT);
-  msg_angle.position[LEFT]  = my_robot->GetEncoderAngle(LEFT);
-  msg_angle.velocity[LEFT]  
-    = (msg_angle.position[LEFT] - prevPosition[LEFT]) / dT;
-
-  msg_angle.name[RIGHT]      = my_robot->GetEncoderTopicName(RIGHT);
-  msg_angle.position[RIGHT]  = my_robot->GetEncoderAngle(RIGHT);
-  msg_angle.velocity[RIGHT]  
-    = (msg_angle.position[RIGHT] - prevPosition[RIGHT]) / dT;
-
-  prevPosition[LEFT]  = msg_angle.position[LEFT];
-  prevPosition[RIGHT] = msg_angle.position[RIGHT];
-
-  prevTime = msg_angle.header.stamp;
-
-  pub.publish(&msg_angle);
-
-  if( !(my_robot->nh).connected() )  my_robot->Stop();
+  	if( !(my_robot->nh).connected() )  my_robot->Stop();
 }
 
 //////////////////////////////////////////////////////////////////
@@ -60,7 +52,7 @@ void loop()
 
 void ddr_callback(const geometry_msgs::Twist& msg_motor) {
 
-	// TODO: WHAT IS THE PURPOSE OF var???
+	// TODO: WHAT IS THE PURPOSE OF var??? For testing...
 
   int var = my_robot->Move(msg_motor.linear.x, msg_motor.angular.z);
 }
