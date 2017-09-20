9748115c7bccd3db22e47d5d88fbe84a73eaf496
Emiliano Borghi <emiliano_borghi@hotmail.com>
2017-09-19 23:48:08 -0300

    Add plugins package

---
+++ my_robot_plugins/include/my_robot_plugins/velodyne_plugin.h
@@ -0,0 +1,31 @@
+#ifndef _VELODYNE_PLUGIN_HH_
+#define _VELODYNE_PLUGIN_HH_
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+
+namespace gazebo
+{
+  /// \brief A plugin to control a Velodyne sensor.
+  class VelodynePlugin : public ModelPlugin
+  {
+    /// \brief Constructor
+    public: VelodynePlugin() {}
+
+    /// \brief The load function is called by Gazebo when the plugin is
+    /// inserted into simulation
+    /// \param[in] _model A pointer to the model that this plugin is
+    /// attached to.
+    /// \param[in] _sdf A pointer to the plugin's SDF element.
+    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+    {
+      // Just output a message for now
+      std::cerr << "\nThe velodyne plugin is attach to model[" <<
+        _model->GetName() << "]\n";
+    }
+  };
+
+  // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
+  GZ_REGISTER_MODEL_PLUGIN(VelodynePlugin)
+}
+#endif
